994cd8bcb7f16b5b78ebeeba6add852dca2ee8e4
Doublepauld <97725022+Doublepauld@users.noreply.github.com>
2024-03-29 02:25:58 +0100

    Přidejte soubory projektu.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVOP_připomínač
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Open_create(object sender, RoutedEventArgs e)
+        {
+            Window1 secondWindow = new Window1();
+
+            secondWindow.OpakovaneUdalostiChanged += Window1_OpakovaneUdalostiChanged;
+            // Subscribe to the event for NeopakovaneUdalosti
+            secondWindow.NeopakovaneUdalostiChanged += Window1_NeopakovaneUdalostiChanged;
+            secondWindow.Show();
+
+            List<Udalost> udalosti = new List<Udalost>(secondWindow.Udalosti);
+            List<Udalost> opakovaneUdalosti = new List<Udalost>(secondWindow.OpakovaneUdalosti);
+            List<Udalost> neopakovaneUdalosti = new List<Udalost>(secondWindow.NeopakovaneUdalosti);
+
+            myListViewOpakovane.DataContext = opakovaneUdalosti;
+            myListViewNeopakovane.DataContext = neopakovaneUdalosti;
+
+        }
+       
+
+        // Event handler for OpakovaneUdalostiChanged event
+        private void Window1_OpakovaneUdalostiChanged(object sender, EventArgs e)
+        {
+            // Refresh the ListView when OpakovaneUdalosti list changes
+            myListViewOpakovane.ItemsSource = (sender as Window1)?.OpakovaneUdalosti;
+        }
+
+        // Event handler for NeopakovaneUdalostiChanged event
+        private void Window1_NeopakovaneUdalostiChanged(object sender, EventArgs e)
+        {
+            // Refresh the ListView when NeopakovaneUdalosti list changes
+            myListViewNeopakovane.ItemsSource = (sender as Window1)?.NeopakovaneUdalosti;
+        }
+
+        
+
+
+    }
+
+
+}
---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MVOP_připomínač
+{
+    /// <summary>
+    /// Interakční logika pro Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public List<Udalost> Udalosti { get; } = new List<Udalost>();
+        public List<Udalost> OpakovaneUdalosti { get; } = new List<Udalost>();
+        public List<Udalost> NeopakovaneUdalosti { get; } = new List<Udalost>();
+
+        public event EventHandler OpakovaneUdalostiChanged;
+        public event EventHandler NeopakovaneUdalostiChanged;
+
+
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+           
+            if (!IsPositiveNumber(e.Text) || (sender as TextBox).Text.Length >= 3)
+            {
+                e.Handled = true; 
+            }
+        }
+
+        bool IsPositiveNumber(string text)
+        {
+            
+            if (double.TryParse(text, out double number))
+            {
+                return number >= 0; 
+            }
+            return false; 
+        }
+
+        private void CreateUdalostButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            string jmeno = Jmeno_udalost.Text;
+            DateTime? selectedDate = Datum_udalost.Value;
+            int warning;
+            bool opakovani = opakovani_udalost.IsChecked ?? false;
+
+            if (!int.TryParse(warning_time.Text, out warning))
+            {
+                
+                MessageBox.Show("Warning time must be a valid integer.");
+                return;
+            }
+
+
+            Udalost newUdalost = new Udalost(jmeno, selectedDate ?? DateTime.MinValue, warning, opakovani);
+
+            // Add the new Udalost to the existing lists
+            Udalosti.Add(newUdalost);
+
+            if (opakovani)
+            {
+                OpakovaneUdalosti.Add(newUdalost);
+                OnOpakovaneUdalostiChanged();
+            }
+            else
+            {
+                NeopakovaneUdalosti.Add(newUdalost);
+                OnNeopakovaneUdalostiChanged();
+            }
+
+
+
+
+            Jmeno_udalost.Text = "";
+            Datum_udalost.Value = null;
+            warning_time.Text = "";
+            opakovani_udalost.IsChecked = false;
+
+            this.Close();
+
+
+
+           
+        }
+
+        protected virtual void OnOpakovaneUdalostiChanged()
+        {
+            OpakovaneUdalostiChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnNeopakovaneUdalostiChanged()
+        {
+            NeopakovaneUdalostiChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+
+    }
+    public class Udalost
+    {
+        string jmeno;
+        public string Jmeno
+        {
+            get { return jmeno; }
+            set { jmeno = value; }
+        }
+
+        DateTime date;
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        DateTime warning;
+        public DateTime Warning
+        {
+            get { return warning; }
+            set { warning = value; }
+        }
+
+        bool opakovani;
+        public bool Opakovani
+        {
+            get { return opakovani; }
+            set { opakovani = value; }
+        }
+
+        public Udalost(string jmeno, DateTime date, int? warningMinutes = null, bool opakovani = false)
+        {
+            this.Jmeno = jmeno;
+            this.Date = date;
+            this.Warning = date.AddMinutes(-(warningMinutes ?? 30));
+            this.Opakovani = opakovani;
+        }
+    }
+
+   
+}
+
